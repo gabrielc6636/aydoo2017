e8b6415e9ab83705cce70882117fbf03e7d45706
gabrielc6636 <gabrielc6636@gmail.com>
2017-04-14 18:54:09 -0300

    EnumMes

---
+++ club-de-beneficios/src/main/java/ar/edu/untref/aydoo/club/EnumMes.java
@@ -0,0 +1,8 @@
+package ar.edu.untref.aydoo.club;
+
+/**
+ * Created by gabriel on 14/04/17.
+ */
+public enum EnumMes {
+    ENERO,FEBRERO,MARZO,ABRIL,MAYO,JUNIO,JULIO,AGOSTO,SEPTIEMBRE,OCTUBRE,NOVIEMBRE,DICIEMBRE;
+}
